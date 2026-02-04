3a7f3465f693f58e9f668e82f5db4e9aa0e7daca
folkert van heusden <folkert@komputilo.nl>
2026-02-04 17:13:07 +0100

    library

--- Examples/2_ChatCommandExample/controller.py
+++ Examples/2_ChatCommandExample/controller.py
@@ -14,10 +14,12 @@ def move_to(base_url, x, y, z):
 def screenshot(base_url):
     return requests.get(base_url + 'screenshot').content
 
-base_url = 'http://minecraft.vm.nurd.space:8080/'
 
-while True:
-    look_at(base_url, random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
-    move_to(base_url, random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
-    print(screenshot(base_url))
-    time.sleep(0.5)
+if __name__ == '__main__':
+    base_url = 'http://minecraft.vm.nurd.space:8080/'
+
+    while True:
+        look_at(base_url, random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
+        move_to(base_url, random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
+        #print(screenshot(base_url))
+        time.sleep(0.5)
